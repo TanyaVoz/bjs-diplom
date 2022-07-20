f5361d4557a559662f87dd5d5506ca5dbbede9c4
Tanya <voznyuk1998@mail.ru>
2022-07-20 20:33:33 +0300

    finish diploma

--- public/homePage.js
+++ public/homePage.js
@@ -30,3 +30,86 @@ function stocks() {
 
 stocks();
 setTimeout(stocks, 60000);
+
+//1.блок операции с деньгами
+const moneyManager = new MoneyManager();
+
+//1.2  пополнение баланса
+moneyManager.addMoneyCallback = (data) => {
+
+    ApiConnector.addMoney(data, response => {  // запрос на пополнение баланса 
+
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Счет был успешно пополнен");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        }
+    });
+}
+
+//1.3 конвертирование валюты
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, response => {
+
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Конвертация прошла успешно");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        }
+    });
+}
+
+//1.4 перевод валюты
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, response => {
+
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Перевод прошел успешно");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        }
+    });
+}
+
+//1.блок "Работа с избранным"
+const favorite = new FavoritesWidget();
+
+//1.2 начальный список избранного
+ApiConnector.getFavorites(function (response) {
+    if (response.success) {
+        favorite.clearTable();
+        favorite.fillTable(response.success);
+        moneyManager.updateUsersList(response.success);
+    }
+});
+
+//1.3 Реализуйте добавления пользователя в список избранных
+favorite.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+
+        if (response.success) {
+            favorite.clearTable();
+            favorite.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favorite.setMessage(response.success, "Пользователь добавлен");
+        } else {
+            favorite.setMessage(response.success, response.error);
+        }
+    });
+}
+//1.4 Реализуйте удаление пользователя из избранного
+favorite.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favorite.clearTable();
+            favorite.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favorite.setMessage(response.success, "Пользователь удален");
+        } else {
+            favorite.setMessage(response.success, response.error);
+        }
+    });
+}
